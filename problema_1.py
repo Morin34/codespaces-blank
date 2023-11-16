6cc06ebaa5aa166a129eb8fe9b5c4edb670777bb
Morin34 <151070402+Morin34@users.noreply.github.com>
2023-11-16 12:50:18 -0600

    Update problema_1.py

--- problema_1.py
+++ problema_1.py
@@ -6,12 +6,12 @@ class Persona:
         print(f"Su nombre es: {self.nombre}")
 print("============Persona numero 1============")        
 persona1 = Persona()
-persona1.inicializar("Miguel")
+persona1.inicializar("Gael")
 persona1.imprimir()
 
 print("================================================================")
 
 print("============Persona numero 2============")
 persona2 = Persona()
-persona2.inicializar("Angel")
+persona2.inicializar("Morin")
 persona2.imprimir()
